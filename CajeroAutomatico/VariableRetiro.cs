9887e27aed256c24b1a12b919dcc699e4568df4a
angelbaezsa <47637565+angelbaezsa@users.noreply.github.com>
2020-06-09 01:24:16 -0400

    Create VariableRetiro.cs

---
+++ CajeroAutomatico/VariableRetiro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CajeroAutomatico
+{
+    public class VariableRetiro
+    {
+        public int OpcionRetiro { get; set; }
+
+        public VariableRetiro(int OpcionRetiro)
+        {
+            this.OpcionRetiro = OpcionRetiro;
+        }
+
+        public static VariableRetiro MetodoRetiro { get; } = new VariableRetiro();
+
+        public VariableRetiro() { }
+    }
+}
