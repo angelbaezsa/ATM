e8ad94b9db57a96d180d6ff5c7ccc8ab109f8b28
angelbaezsa <47637565+angelbaezsa@users.noreply.github.com>
2020-06-09 01:14:12 -0400

    Corregido errores menu
    funcionalidad volver a menu agregada

---
+++ CajeroAutomatico/CajeroAutomatico/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CajeroAutomatico
+{
+    public class MenuPrincipal
+    {
+
+        public void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Seleccione una opcion: \n 1- Retiro\n 2- Modo de retiro\n 3- Salir");
+
+
+            try
+            {
+                
+                int opcion = Convert.ToInt32(Console.ReadLine());
+                
+
+                switch (opcion)
+                {
+                    case (int) opcionMenu.RETIRO:
+                        Retiro menu = new Retiro();
+                        menu.RetirarRapido();
+                        break;
+                    case (int)opcionMenu.MODORETIRO:
+                        Modo_Retiro menuModoRetiro = new Modo_Retiro();
+                        menuModoRetiro.MenuModoRetiros();
+                        break;
+                    case (int)opcionMenu.SALIR:
+                        Console.WriteLine("Gracias por usar nuestros servicios");
+                        Console.ReadKey();
+                        MainMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Seleccione una opcion validad");
+                        Console.ReadKey();
+                        MainMenu();
+                        break;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Selecciona una opcion valida");
+                Console.ReadKey();
+                MainMenu();
+                throw;
+            }
+        }
+
+        enum opcionMenu
+        {
+            RETIRO = 1, MODORETIRO, SALIR
+        }
+            
+     
+    }
+
+
+}
+
+
+
+    
+
+
---
+++ CajeroAutomatico/CajeroAutomatico/Modo Retiro.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CajeroAutomatico
+{
+    public class Modo_Retiro
+    {
+
+
+        enum opcionesMenuRetiro { OPCION1 = 1, OPCION2, OPCION3 }
+
+
+
+
+        public void MenuModoRetiros()
+          {
+
+                Console.WriteLine("Seleccione una opcion");
+                Console.WriteLine("Modo de cambio: ");
+                Console.WriteLine("1- 100-500\n2- 200-1000\n3- Retirar");
+                int opcionMenuRetiro = Convert.ToInt32(Console.ReadLine());
+                VariableRetiro.MetodoRetiro.OpcionRetiro = opcionMenuRetiro;
+
+
+            try
+                {
+
+                    
+
+                    switch (opcionMenuRetiro)
+                    {
+                        case (int)opcionesMenuRetiro.OPCION1:
+                        MenuPrincipal retirar1 = new MenuPrincipal();
+                        retirar1.MainMenu();
+                        VariableRetiro.MetodoRetiro.OpcionRetiro = 1;
+                        
+
+                        break;
+                        case (int)opcionesMenuRetiro.OPCION2:
+                        MenuPrincipal retirar2 = new MenuPrincipal();
+                        retirar2.MainMenu();
+                        VariableRetiro.MetodoRetiro.OpcionRetiro = 2;
+
+                        break;
+                        case (int)opcionesMenuRetiro.OPCION3:
+                        MenuPrincipal retirar3 = new MenuPrincipal();
+                        retirar3.MainMenu();
+                        VariableRetiro.MetodoRetiro.OpcionRetiro = 3;
+
+                        break;
+                        default:
+                            Console.WriteLine("seleccione una opcion correcta");
+                            Console.ReadKey();
+                            MenuModoRetiros();
+                            break;
+
+                    }
+
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Seleccione una opcion valida");
+                    Console.ReadKey();
+                    MenuModoRetiros();
+                }
+
+
+                
+            
+
+
+          }
+       
+
+
+    }
+}
